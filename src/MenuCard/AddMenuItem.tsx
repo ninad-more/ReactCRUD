ebc34394aa2180ced074ac33cba4fce749d7fe57
ninad.more.github@gmail.com <ninad.more.github@gmail.com>
2022-07-26 00:39:13 +0530

    imported and used yup library for validation

--- src/MenuCard/AddMenuItem.tsx
+++ src/MenuCard/AddMenuItem.tsx
@@ -1,7 +1,7 @@
-import { Field } from "formik";
-import { Formik, Form } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "../Utils/Button";
+import * as Yup from "yup";
 
 var AddMenuItem = () => {
   const navigate = useNavigate();
@@ -14,17 +14,27 @@ var AddMenuItem = () => {
         onSubmit={(value) => {
           console.log(value);
         }}
+        validationSchema={Yup.object({
+          name: Yup.string().required("Name is mandetory"),
+          poster: Yup.string().required("Poster is mandetory"),
+        })}
       >
         <Form>
           <div className="mb-3">
             <label htmlFor="name">Name</label>
             <Field id="name" name="name" className="form-control" />
+            <ErrorMessage name="name">
+              {(msg) => <div className="alert alert-danger">{msg}</div>}
+            </ErrorMessage>
           </div>
+
           <div className="mb-3">
             <label htmlFor="poster">Image Link</label>
             <Field id="poster" name="poster" className="form-control" />
+            <ErrorMessage name="poster">
+              {(msg) => <div className="alert alert-danger">{msg}</div>}
+            </ErrorMessage>
           </div>
-
           <Button type="submit">Add</Button>
           <Link className="btn btn-secondary" to="/">
             Cancel
